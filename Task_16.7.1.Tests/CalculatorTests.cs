20881fde49201ab74f739dfec2c6f5dbcdb1190b
susanoo-10011 <cycahosir@gmail.com>
2023-07-14 11:18:04 +0700

    Добавьте файлы проекта.

---
+++ Task_16.7.1.Tests/CalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Task_16._7._1.Tests
+{
+        [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Additional(5, 3) == 8);
+        }
+
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Subtraction(5, 3) == 2);
+        }
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(5, 3) == 15);
+        }
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(6, 3) == 2);
+        }
+    }
+}
---
+++ Task_16.7.1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Task_16._7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
